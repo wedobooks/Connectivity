4f5d16e0abe12a624ec706bace368952658b1b63
Uffe Sørensen <datalogen@gmail.com>
2020-09-21 16:11:00 +0200

    Added xcodeproj

---
+++ Connectivity-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Connectivity-Bridging-Header.h
+//  Connectivity
+//
+//  Created by Uffe Sørensen on 21/09/2020.
+//  Copyright © 2020 Uffe Sørensen. All rights reserved.
+//
+
+#ifndef Connectivity_Bridging_Header_h
+#define Connectivity_Bridging_Header_h
+
+#import "Reachability.h"
+#endif /* Connectivity_Bridging_Header_h */
